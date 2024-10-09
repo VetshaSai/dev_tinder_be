a6fd15664bfb078533336b1ff9e0aa96c9c7c774
Sai Durgesh <durgarao.vetsha@gmail.com>
2024-10-09 06:30:09 +0530

    playing with routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("test test");
+// app.use("/test", (req, res) => {
+//   res.send("test test");
+// });
+
+app.get("/user", (req, res)=>{
+    res.send([{name:"Sai", Salary: 2500000},{name:"Ram", Salary: 5500000}]);
 });
 
-app.use("/hello", (req, res)=>{
-    res.send("Helloooooo");
+app.post("/user",(req, res)=>{
+  res.send("post data sent sucessfully");
 });
+
+app.delete("/user",(req,res)=>{
+  res.send("Data deleted sucessfully...")
+})
 
 app.use("/mass",(req,res)=>{
     res.send("Mama mass ey ra bamaradi");
 });
 
-app.use("/", (req, res) => {
-    res.send("Namste !!!!");
-  });
+app.get("/abcd/:userId/:userName", (req,res)=>{
+  console.log(req.params);
+  res.send("Good Morningggg "+req.params.userName +"!!");
+})
+// app.use("/", (req, res) => {
+//     res.send("Namste !!!!");
+//   });
 
 app.listen(7777, () => {
   console.log("server is listening on port number 7777");
