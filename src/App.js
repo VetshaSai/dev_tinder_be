fb41755c18ac353cbac9db33d65d6c6327c358ad
Sai Durgesh <durgarao.vetsha@gmail.com>
2024-10-10 13:02:13 +0530

    Wrapping route handlers in array

--- src/App.js
+++ src/App.js
@@ -5,26 +5,25 @@ const app = express();
 //   res.send("test test");
 // });
 
-app.get("/user", (req, res)=>{
-    res.send([{name:"Sai", Salary: 2500000},{name:"Ram", Salary: 5500000}]);
-});
+app.use(
+  "/user",
 
-app.post("/user",(req, res)=>{
-  res.send("post data sent sucessfully");
-});
+  [(req, res, next) => {
+    next();
+    //res.send("hiiiiii");
+  },
+  (req, res, next) => {
+    next();
+    //res.send("2nd response");
+  }],
+  (req, res) => {
+    res.send("3rd Response");
+  })
 
-app.delete("/user",(req,res)=>{
-  res.send("Data deleted sucessfully...")
-})
-
-app.use("/mass",(req,res)=>{
-    res.send("Mama mass ey ra bamaradi");
-});
-
-app.get("/abcd/:userId/:userName", (req,res)=>{
-  console.log(req.params);
-  res.send("Good Morningggg "+req.params.userName +"!!");
-})
+// app.get("/abcd/:userId/:userName", (req,res)=>{
+//   console.log(req.params);
+//   res.send("Good Morningggg "+req.params.userName +"!!");
+// })
 // app.use("/", (req, res) => {
 //     res.send("Namste !!!!");
 //   });
